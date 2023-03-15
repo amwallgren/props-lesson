5eeb6589f9e3cd89ab37bf286dc9218e26a4ab55
A. Mirelle Wallgren <mirelle.wallgren@medieinstitutet.se>
2023-03-15 15:08:17 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Add from './components/Add.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Add x = {10} y = {20}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Props/Add.jsx
@@ -0,0 +1,22 @@
+import './../style/add.css';
+
+   export function Add(props) {
+      //  props.x -> 10
+      //  props.y -> 20
+
+ //  function AddAdvanced({x, y}){
+        // Olika sätt att skriva samma sak. 
+        // x -> 10
+        // y -> 20
+//    };
+        return (
+            <>
+            <h1>{props.x} - {props.y}</h1>
+            </>
+        );
+}
+
+// props och objekt 
+export function Object() {
+
+}
